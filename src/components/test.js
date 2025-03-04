41c1e24ac539983467cad272dd9c6c11b9e3d4d6
nikk0895 <newnikk08@gmail.com>
2025-03-04 23:49:58 +0530

    Inserting the User record in Tabular way

--- src/components/test.js
+++ src/components/test.js
@@ -1,7 +1,6 @@
 import react, { useState } from "react";
 
 const Test = () => {
-  // const [name, setName] = useState();
   const [userArr, setUserArr] = useState([]);
   const [formData, setFormData] = useState({
     name: "",
@@ -11,47 +10,20 @@ const Test = () => {
     gpa: "",
   });
 
-  const user = JSON.parse(localStorage.getItem("userData"));
-  console.log("The values we feteched", user);
-
   const handleSave = (e) => {
     e.preventDefault();
-    // console.log("Submitted Data:", userArr);
     console.log("Submitted Data:", formData);
-
-    console.log("the Name you entered :", formData.name);
-    userArr.push(formData.name);
-    userArr.push(formData.degree);
-    userArr.push(formData.institution);
-    userArr.push(formData.graduationYear);
-    userArr.push(formData.gpa);
-    console.log("the check", userArr);
-    setFormData("");
+    setUserArr((prev) => [...prev, formData]);
+    console.log("userArr", userArr);
   };
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   console.log("Submitted Data:", formData);
-  // };
-
   return (
-    // <div style={{ marginLeft: "100px" }}>
-    //   <p>Hello Write </p>;
-    //   <input
-    //     type="text"
-    //     placeholder="Enter your Name"
-    //     value={name}
-    //     required
-    //     onChange={(event) => {
-    //       setName(event.target.value);
-    //     }}
-    //   />
     <div style={{ marginLeft: "370px" }}>
-      <h2>Student Education Details</h2>
+      <h2>Student Education Detail</h2>
       <form onSubmit={handleSave} className="space-y-3">
         <input
           type="text"
@@ -92,39 +64,40 @@ const Test = () => {
           value={formData.gpa}
           onChange={handleChange}
         />
-        <button onClick={handleSave} style={{ marginLeft: "130px" }}>
+        <button type="submit" style={{ marginLeft: "130px", width: "200px" }}>
           Save
         </button>
-        {/* To Display Users DATA */}
-        <div>
-          {userArr.length > 0 ? (
-            <div>
-              {userArr.map((user, index) => {
-                return <span key={index}>{user + ","}</span>;
-              })}
-            </div>
-          ) : (
-            <tbody>
-              <tr>
-                <th>Name</th>
-                <th>Degree</th>
-                <th>Institution</th>
-                <th>Graduation Year</th>
-                <th>GPA</th>
-              </tr>
-              {userArr.map((item, i) => (
-                <tr key={i}>
-                  <td>{item.name}</td>
-                  <td>{item.degree}</td>
-                  <td>{item.institution}</td>
-                  <td>{item.graduationYear}</td>
-                  <td>{item.gpa}</td>
-                </tr>
-              ))}
-            </tbody>
-          )}
-        </div>
       </form>
+
+      {/* To Display Users DATA */}
+
+      {userArr.length > 0 ? (
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Degree</th>
+              <th>Institution</th>
+              <th>Graduation Year</th>
+              <th>GPA</th>
+            </tr>
+
+            {userArr.map((user, index) => {
+              return (
+                <tr key={index}>
+                  <td>{user.name}</td>
+                  <td>{user.degree}</td>
+                  <td>{user.institution}</td>
+                  <td>{user.graduationYear}</td>
+                  <td>{user.gpa}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      ) : (
+        <div>No Record Found</div>
+      )}
     </div>
   );
 };
